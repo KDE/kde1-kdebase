1201e0ec815e4ebc196b9974f5594f28ec3f3170
KDE Dinosaur cvsuser <cvsuser@kde.org>
1997-04-13 14:48:34 +0000

    Source imported

---
+++ config.h
@@ -0,0 +1,77 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if on AIX 3.
+   System headers sometimes define this.
+   We just want to avoid a redefinition error message.  */
+#ifndef _ALL_SOURCE
+/* #undef _ALL_SOURCE */
+#endif
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as __inline if that's what the C compiler calls it.  */
+/* #undef inline */
+
+/* Define if on MINIX.  */
+/* #undef _MINIX */
+
+/* Define if the system does not provide POSIX.1 features except
+   with this defined.  */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define if you need to in order for stat and other things to work.  */
+/* #undef _POSIX_SOURCE */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if the C++ compiler supports BOOL */
+#define HAVE_BOOL 1
+
+/* Define if you have the socket function.  */
+/* #undef HAVE_SOCKET */
+
+/* Define if you have the <dirent.h> header file.  */
+#define HAVE_DIRENT_H 1
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <ndir.h> header file.  */
+/* #undef HAVE_NDIR_H */
+
+/* Define if you have the <sys/dir.h> header file.  */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define if you have the <sys/ndir.h> header file.  */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the compat library (-lcompat).  */
+/* #undef HAVE_LIBCOMPAT */
+
+/* Define if you have the gif library (-lgif).  */
+#define HAVE_LIBGIF 1
+
+/* Define if you have the jpeg library (-ljpeg).  */
+#define HAVE_LIBJPEG 1
+
+/* Define if you have the qt library (-lqt).  */
+#define HAVE_LIBQT 1
+
+#ifndef HAVE_BOOL
+#define HAVE_BOOL
+typedef int bool;
+const bool false = 0;
+const bool true = 1;
+#endif
