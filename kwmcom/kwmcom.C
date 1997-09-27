9204389a5f17d4f5512d107f609126d3f1b28bc2
KDE Dinosaur ettrich <cvsuser@kde.org>
1997-09-27 11:19:59 +0000

    Matthias: kwmcom (will replace kwmclient)

---
+++ kwmcom/kwmcom.C
@@ -0,0 +1,25 @@
+/*
+ * kwmcom.C. Part of the KDE project.
+ *
+ * Copyright (C) 1997 Matthias Ettrich
+ *
+ */
+
+#include <qapp.h>
+#include <qstring.h>
+#include <kwm.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+  QApplication a(argc, argv);
+  
+  if (argc != 2){
+    printf("Usage: ");
+    printf("%s <command>\n", argv[0]);
+    exit(1);
+  }
+  else {
+    KWM::sendKWMCommand(argv[1]);
+  }
+  return 0;
+}
