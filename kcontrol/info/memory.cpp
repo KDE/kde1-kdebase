3cb41230f08402f1dc797e7910d03271d949e80c
KDE Dinosaur jacek <cvsuser@kde.org>
1998-01-25 15:21:23 +0000

    Bug fix:   Font setting - default charset used

--- kcontrol/info/memory.cpp
+++ kcontrol/info/memory.cpp
@@ -1,5 +1,6 @@
 #include <qtabbar.h>
 #include <kapp.h>
+#include <kcharsets.h>
 #include "memory.h"
 #include "memory.moc"
 
@@ -13,7 +14,9 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   totalMem = new QLabel("0k", this);
   totalMem->move(200,8);
   totalMem->setAutoResize(TRUE);
-  totalMem->setFont(QFont("Courier"));
+  QFont courierFont("Courier");
+  KApplication::getKApplication()->getCharsets()->setQFont(courierFont);
+  totalMem->setFont(courierFont);
 
   freeMem = new QLabel(klocale->translate("Free memory"), this);
   freeMem->move(8,32);
@@ -21,7 +24,7 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   freeMem = new QLabel("0k", this);
   freeMem->move(200,32);
   freeMem->setAutoResize(TRUE);
-  freeMem->setFont(QFont("Courier"));
+  freeMem->setFont(courierFont);
 
   sharedMem = new QLabel(klocale->translate("Shared memory"), this);
   sharedMem->move(8,56);
@@ -29,7 +32,7 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   sharedMem = new QLabel("0k", this);
   sharedMem->move(200,56);
   sharedMem->setAutoResize(TRUE);
-  sharedMem->setFont(QFont("Courier"));
+  sharedMem->setFont(courierFont);
 
   bufferMem = new QLabel(klocale->translate("Buffer memory"), this);
   bufferMem->move(8,80);
@@ -37,7 +40,7 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   bufferMem = new QLabel("0k", this);
   bufferMem->move(200,80);
   bufferMem->setAutoResize(TRUE);
-  bufferMem->setFont(QFont("Courier"));
+  bufferMem->setFont(courierFont);
 
   swapMem = new QLabel(klocale->translate("Swap memory"), this);
   swapMem->move(8,112);
@@ -45,7 +48,7 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   swapMem = new QLabel("0k", this);
   swapMem->move(200,112);
   swapMem->setAutoResize(TRUE);
-  swapMem->setFont(QFont("Courier"));
+  swapMem->setFont(courierFont);
 
   freeSwapMem = new QLabel(klocale->translate("Free swap memory"), this);
   freeSwapMem->move(8,136);
@@ -53,7 +56,7 @@ KMemoryWidget::KMemoryWidget(QWidget *parent, const char *name)
   freeSwapMem = new QLabel("0k", this);
   freeSwapMem->move(200,136);
   freeSwapMem->setAutoResize(TRUE);
-  freeSwapMem->setFont(QFont("Courier"));
+  freeSwapMem->setFont(courierFont);
 
   timer = new QTimer(this);
   timer->start(100);
