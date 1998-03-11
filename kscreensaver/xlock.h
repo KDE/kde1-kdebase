f9874206cc21eed6c2c4f95066d8598ab894110e
KDE Dinosaur wuebben <cvsuser@kde.org>
1998-03-11 05:28:50 +0000

    *** empty log message ***

--- kscreensaver/xlock.h
+++ kscreensaver/xlock.h
@@ -70,9 +70,17 @@ void initXLock( GC gc );
 #endif
 #endif
 
+#include <math.h>
+
+#ifndef M_E
 #define M_E    2.7182818284590452354
+#endif
+#ifndef M_PI
 #define M_PI   3.14159265358979323846
+#endif
+#ifndef M_PI_2
 #define M_PI_2 1.57079632679489661923
+#endif
 
 #if !defined (news1800) && !defined (sun386)
 #include <stdlib.h>
