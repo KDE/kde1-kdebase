7f9c2f06e646c1be85d962fb12bc31a5812ccede
KDE Dinosaur ettrich <cvsuser@kde.org>
1998-05-16 08:32:01 +0000

    Matthias: sorry %-}

---
+++ kwm/warning.C
@@ -0,0 +1,137 @@
+// klogout 
+// Copyright (C) 1997 Matthias Ettrich
+
+#include "warning.moc"
+#include <kapp.h>
+#include <qwidget.h>
+#include <qpainter.h>
+#include <qbitmap.h>
+#include <qwindefs.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#include <kapp.h>
+#include <kcharsets.h>
+
+#include "manager.h"
+
+extern Manager* manager;
+
+extern bool do_not_draw;
+
+
+KWarning::KWarning( QWidget *parent, const char *name, WFlags f)
+  : QDialog(parent, name, False, f){
+    setMouseTracking(True);
+    frame = new QFrame( this );
+    frame->installEventFilter( this );
+    frame->setMouseTracking(True);
+    frame->setFrameStyle(QFrame::WinPanel | QFrame:: Raised);
+    button = new QPushButton(klocale->translate("Ooops!"), this);
+    button->setMouseTracking(True);
+    button->setDefault( True );
+    installEventFilter( this );
+    button->installEventFilter( this );
+    connect(button, SIGNAL(clicked()), SLOT(ok()));
+    label = new QLabel(this);
+    label->setAlignment(AlignCenter);
+}
+
+void KWarning::SetPointerGrab(QPoint pos){
+  QWidget* w = QApplication::widgetAt( pos, true);
+  if (!w)
+    return;
+  if (w->topLevelWidget() == this){
+    if (w != mouseGrabber()){
+      mouseGrabber()->releaseMouse();
+      w->removeEventFilter(this);
+      w->installEventFilter(this);
+      w->setMouseTracking(true);
+      w->grabMouse();
+    }
+  }
+}
+
+bool KWarning::eventFilter( QObject *ob, QEvent * e){
+  if (e->type() == Event_MouseButtonPress){
+    if (ob->isWidgetType() && 
+	!rect().contains(
+			 mapFromGlobal(
+				       ((QWidget*)ob)->mapToGlobal(
+								   ((QMouseEvent*)e)->pos())))){
+      ok();
+    }
+  }
+  if (e->type() == Event_KeyPress){
+    int a = ((QKeyEvent*)e)->ascii();
+    if (a == 3 || a == 7 || a == 27)
+      ok();
+  }
+  if (e->type() == Event_MouseMove){
+    QMouseEvent* mev = (QMouseEvent*) e;
+    if (ob->isWidgetType()
+	&& !(mev->state() & LeftButton)
+	&& !(mev->state() & MidButton)
+	&& !(mev->state() & RightButton)
+	){
+      SetPointerGrab(((QWidget*)ob)->mapToGlobal(mev->pos()));
+    }
+  }
+  return False;
+}
+
+void KWarning::setText(const char* text){
+  label->setText(text);
+  label->adjustSize();
+  int w = QMAX(label->width() + 20, 100);
+  setGeometry(QApplication::desktop()->width()/2-w/2,
+	      QApplication::desktop()->height()/2-label->height()/2-40,
+	      w, label->height()+80);
+  label->setGeometry(4,10,w-8,label->height());
+  button->setGeometry(w/2-30, label->height()+20, 60, 30);
+  frame->setGeometry(0,0, width(), height());
+}
+
+bool KWarning::do_grabbing(){
+  reactive = manager->current();
+  if (reactive)
+    reactive->setactive(False);
+  manager->darkenScreen();
+  XGrabServer(qt_xdisplay());
+  do_not_draw = true;
+  show();
+  XSetInputFocus (qt_xdisplay(), winId(), RevertToParent, CurrentTime);
+  if (XGrabKeyboard(qt_xdisplay(), winId(),True,GrabModeAsync,
+  		    GrabModeAsync,CurrentTime) != GrabSuccess){
+    XUngrabServer(qt_xdisplay());
+    return False;
+  }
+  raise();
+  button->grabMouse();
+  SetPointerGrab(QCursor::pos());
+  button->setFocus();
+  
+  return True;
+}
+
+void KWarning::ok(){
+  XUngrabServer(qt_xdisplay());
+  if (mouseGrabber())
+    mouseGrabber()->releaseMouse();
+  hide();
+  do_not_draw = false;
+  if (reactive){
+    reactive->setactive(True);
+    XSetInputFocus (qt_xdisplay(), reactive->window, 
+		    RevertToPointerRoot, CurrentTime);
+  }
+  manager->refreshScreen();
+}
+
+
---
+++ kwm/warning.h
@@ -0,0 +1,36 @@
+// klogout 
+// Copyright (C) 1997 Matthias Ettrich
+
+#include <qmsgbox.h>
+#include <qlined.h>
+#include <qlabel.h>
+#include <qframe.h>
+#include <qdialog.h>
+#include <qpushbt.h>
+#include <qlistbox.h>
+#include <qstring.h>
+
+#include <qapp.h>
+#include <qwindefs.h>
+#include <X11/Xlib.h>
+#include <stdlib.h>
+
+#include "client.h"
+
+
+class KWarning : public QDialog{
+  Q_OBJECT
+public:
+  KWarning( QWidget *parent=0, const char *name=0, WFlags f=0);
+  bool do_grabbing();
+  void setText(const char* text);
+  bool eventFilter( QObject *, QEvent * );
+public slots:
+  void ok();
+private:
+  QFrame *frame;
+  QPushButton* button;
+  QLabel* label;
+  void SetPointerGrab(QPoint);
+  Client* reactive;
+};
