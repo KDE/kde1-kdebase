4ec0e4a7e6910bc61150909e827aa817837db9d9
KDE Dinosaur ettrich <cvsuser@kde.org>
1998-05-29 07:40:53 +0000

    Matthias: - small fix to avoid a double calling of Manager::sendConfig after a   drag/resize in opaque mode(Matthias)

--- kwm/drag.C
+++ kwm/drag.C
@@ -332,6 +332,14 @@ bool sweepdrag(Client* c,void (*recalc)( Client *, int, int) ){
       if ( other == c->geometry)
 	continue;
       c->geometry = other;
+      if (c->geometry.width() < 0) {
+	c->geometry.moveBy(c->geometry.width(),0);
+	c->geometry.setWidth(  -c->geometry.width());
+      }
+      if (c->geometry.height() < 0) {
+	c->geometry.moveBy(0, c->geometry.height());
+	c->geometry.setHeight(-c->geometry.height());
+      }
       if (transparent && !do_not_clear_rectangle)
 	drawbound(c);
       do_not_clear_rectangle = false;
@@ -352,27 +360,11 @@ bool sweepdrag(Client* c,void (*recalc)( Client *, int, int) ){
       continue;
     }
 
-    if (transparent)
+    if (transparent){
       drawbound(c);
-    
-    if (c->geometry.width() < 0) {
-      c->geometry.moveBy(c->geometry.width(),0);
-      c->geometry.setWidth(  -c->geometry.width());
-    }
-    if (c->geometry.height() < 0) {
-      c->geometry.moveBy(0, c->geometry.height());
-      c->geometry.setHeight(-c->geometry.height());
-    }
-
-    if (options.WindowSnapZone || options.BorderSnapZone) 
-      manager->snapIt(c);
-
-    manager->sendConfig(c);
-    
-    if (transparent)
+      manager->sendConfig(c);
       XUngrabServer(qt_xdisplay());
-
-    while (XCheckMaskEvent(qt_xdisplay(), EnterWindowMask, &ev));
+    }
 
     if (c->isMaximized()){
       c->maximized = false;
