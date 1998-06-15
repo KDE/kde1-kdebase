6456da69eaacefee1d3576414a9b446d1dc216bd
KDE Dinosaur berberic <cvsuser@kde.org>
1998-06-15 11:36:21 +0000

    Now the Terminal-Dialog exchanges the meaning of Backspace and Delete This prevents us from having two Delete-keys and no Backspace-key (bmg)

--- kvt/command.c
+++ kvt/command.c
@@ -724,7 +724,7 @@ static unsigned char *lookup_key(XEvent *ev,int *pcount, unsigned char qt_c)
     case XK_KP_Delete :
 #endif
       /* strcpy(kbuf,"\033[3~"); */
-      strcpy(kbuf, keys[KeySetSend].del);
+      strcpy(kbuf, BackspaceSendsControlH ? "\177" : "\010");
       count = strlen(kbuf);
       break;
 
