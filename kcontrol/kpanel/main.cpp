96f7f53f06d102b9af09a86783614fe4b8e8fd94
KDE Dinosaur ettrich <cvsuser@kde.org>
1998-10-05 18:04:32 +0000

    Matthias: fixed wrong initial geomtry in stand-alone usage

--- kcontrol/kpanel/main.cpp
+++ kcontrol/kpanel/main.cpp
@@ -2,21 +2,21 @@
   main.cpp - KPanel configuration module
 
   written 1997 by Stephan Kulow
-  
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-   
+
   */
 
 
@@ -35,10 +35,10 @@ class KKPanelApplication : public KControlApplication
 public:
 
     KKPanelApplication(int &argc, char **arg, const char *name);
-    
+
     virtual void init();
     virtual void apply();
-    
+
 private:
 
     KPanelConfig *panel;
@@ -51,7 +51,7 @@ void sd(const QSize& r) {
     debug("size %d %d",r.width(), r.height());
 }
 
-KKPanelApplication::KKPanelApplication(int &argc, char **argv, 
+KKPanelApplication::KKPanelApplication(int &argc, char **argv,
 				       const char *name)
     : KControlApplication(argc, argv, name)
 {
@@ -60,7 +60,7 @@ KKPanelApplication::KKPanelApplication(int &argc, char **argv,
     if (runGUI())
 	{
 	    if (!pages || pages->contains("panel"))
-		addPage(panel = new KPanelConfig(dialog, "panel"), 
+		addPage(panel = new KPanelConfig(dialog, "panel"),
 			i18n("&Panel"), "kpanel-1.html");
 	    if (!pages || pages->contains("options"))
 		addPage(options = new KOptionsConfig(dialog, "options"),
@@ -71,45 +71,15 @@ KKPanelApplication::KKPanelApplication(int &argc, char **argv,
 	    if (!pages || pages->contains("disknav"))
 		addPage(disknav = new KDiskNavConfig(dialog, "disknav"),
 			i18n("Disk &Navigator"), "kdisknav.html");
-	    
+	
 	    if (panel || desktops || options || disknav) {
-		QSize t;
-		int w = 0, h = 0;
-		
-		if (panel) {
-		    t = panel->minimumSize();
-		    w = t.width();
-		    h = t.height();
-		}
-		if (desktops) {
-		    t = desktops->minimumSize();
-		    if (w < t.width())
-			w = t.width();
-		    if (h < t.height())
-			h = t.height();
-		}
-		if (options) {
-		    t = options->minimumSize();
-		    if (w < t.width())
-			w = t.width();
-		    if (h < t.height())
-			h = t.height();
-		}
-		if (disknav) {
-		    t = disknav->minimumSize();
-		    if (w < t.width())
-			w = t.width();
-		    if (h < t.height())
-			h = t.height();
-		}
-		dialog->resize(w,h);
 		dialog->show();
 	    }
 	    else {
 		fprintf(stderr, i18n("usage: kcmkpanel [-init | {panel,options,desktops,disknav}]\n"));
 		justInit = true;
 	    }
-	    
+	
 	}
 }
 
@@ -135,10 +105,10 @@ void KKPanelApplication::apply()
 
 int main(int argc, char **argv)
 {
-    config = new KConfig(KApplication::kde_configdir() + "/kpanelrc", 
+    config = new KConfig(KApplication::kde_configdir() + "/kpanelrc",
                          KApplication::localconfigdir() + "/kpanelrc");
     KKPanelApplication app(argc, argv, "kcmkpanel");
-    
+
     app.setTitle(i18n("KPanel Configuration"));
 
     int ret;
