5a2d41f362316e8d6969ad2f5ecce921919e5365
KDE Dinosaur garbanzo <cvsuser@kde.org>
1998-10-11 05:33:32 +0000

    Add back in this code.  It was a popen() problem in fbsd-current that has since been fixed.  Pointy hats all around.

--- kcontrol/info/memory_fbsd.cpp
+++ kcontrol/info/memory_fbsd.cpp
@@ -1,20 +1,23 @@
+// $Id$
+
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <stdlib.h>	// For atoi()
 
 void KMemoryWidget::update()
 {
-  int mib[2],memory;size_t len;char blah[10];
+  char blah[10], buf[80], *used_str, *total_str;
+  /* Stuff for sysctl */
+  int mib[2],memory;size_t len;
   /* Stuff for swap display */
   int used, total, _free;
   FILE *pipe;
-  char buf[80];
   
   mib[0]=CTL_HW;mib[1]=HW_PHYSMEM;
   len=sizeof(memory);
-  sysctl(mib,2,&memory,&len,NULL,0);
+  sysctl(mib, 2, &memory, &len, NULL, 0);
   
-  snprintf(blah,10,"%d",memory);
+  snprintf(blah, 10, "%d", memory);
   // Numerical values
   totalMem->setText(format(memory));
   /*	To: questions@freebsd.org
@@ -22,6 +25,26 @@ void KMemoryWidget::update()
   freeMem->setText(i18n("Not available"));
   sharedMem->setText(i18n("Not available"));
   bufferMem->setText(i18n("Not available"));
-  swapMem->setText(i18n("Not available"));
-  freeSwapMem->setText(i18n("Not available"));
+
+  /* Q&D hack for swap display. Borrowed from xsysinfo-1.4 */
+  if ((pipe = popen("/usr/sbin/pstat -ks", "r")) == NULL) {
+     used = total = 1;
+     return;
+  }
+
+  fgets(buf, sizeof(buf), pipe);
+  fgets(buf, sizeof(buf), pipe);
+  fgets(buf, sizeof(buf), pipe);
+  fgets(buf, sizeof(buf), pipe);
+  pclose(pipe);
+
+  strtok(buf, " ");
+  total_str = strtok(NULL, " ");
+  used_str = strtok(NULL, " ");
+  used = atoi(used_str);
+  total = atoi(total_str); 
+
+  _free=total-used;
+  swapMem->setText(format(1024*total));
+  freeSwapMem->setText(format(1024*_free));
 }
