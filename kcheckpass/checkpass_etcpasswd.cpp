ba89497c6c6053aa149241dfb63e02e6af0e0e23
KDE Dinosaur garbanzo <cvsuser@kde.org>
1998-12-05 07:34:29 +0000

    Nothing really useful.  Who are these files copyrighted by anyways?

--- kcheckpass/checkpass_etcpasswd.cpp
+++ kcheckpass/checkpass_etcpasswd.cpp
@@ -13,11 +13,17 @@
  * License along with this program; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "kcheckpass.h"
+
 #ifdef HAVE_ETCPASSWD
 
 /*******************************************************************
- * This is the authentication code for /etc/passwd Passwords
+ * This is the authentication code for /etc/passwd passwords
  *******************************************************************/
 
 #include <string.h>
@@ -25,19 +31,29 @@
 int authenticate(const char *login, const char *passwd)
 {
   struct passwd *pw;
-  char* crpt_passwd;
+  char *crpt_passwd;
   int result;
 
+  // Get the password entry for the user we want
   pw = getpwnam(login);
-  if ( pw == 0 )
+
+  // getpwnam should return a NULL pointer on error
+  if (pw == 0)
     return 0;
-  crpt_passwd = crypt(passwd,pw->pw_passwd);
-  result = strcmp(pw->pw_passwd, crpt_passwd );
+
+  // Encrypt the password the user entered
+  crpt_passwd = crypt(passwd, pw->pw_passwd);
+
+  // Are they the same?
+  result = strcmp(pw->pw_passwd, crpt_passwd);
+
+  // Close the password file (or database)
   endpwent();
+
   if (result == 0)
     return 1; // success
   else
-    return 0;
+    return 0; // Oops something went wrong, perhaps no user was found?
 }
 
 #endif
