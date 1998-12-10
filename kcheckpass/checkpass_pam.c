50105dd086d96002e36313a49ba332f94928dcc4
KDE Dinosaur esken <cvsuser@kde.org>
1998-12-10 00:09:24 +0000

    Copyright headers added

--- kcheckpass/checkpass_pam.c
+++ kcheckpass/checkpass_pam.c
@@ -12,6 +12,8 @@
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *      Copyright (C) 1998, Caldera, Inc.
  */
 #include "kcheckpass.h"
 #ifdef HAVE_PAM
