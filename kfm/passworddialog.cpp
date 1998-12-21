ffcea1911881a9243e64833412df7f0ee8cc71f5
KDE Dinosaur dfaure <cvsuser@kde.org>
1998-12-21 22:14:52 +0000

    Added layout to the ftp password dialog box, as requested by Waldo Bastian.

--- kfm/passworddialog.cpp
+++ kfm/passworddialog.cpp
@@ -24,26 +24,41 @@ PasswordDialog::PasswordDialog(const char *head, QWidget* parent, const char* na
     
    _head = head;
 
+   // (David) Layout management
+   QVBoxLayout *vLay = new QVBoxLayout(this, 10 /* border */, 5);
+
    //
    // Bei Bedarf einen kleinen Kommentar als Label einfuegen
-   //
+   // English please !!! David.
    if (_head)
    {
       QLabel *l;
       
       l = new QLabel(_head, this);
-      l->setGeometry( 10, 10, 200, 20 );
+      l->adjustSize();
+      l->setMinimumSize(l->size());
+      vLay->addWidget(l);
+      // l->setGeometry( 10, 10, 200, 20 );
    }
-   
+
+   // The horizontal layout for label + lineedit
+   QHBoxLayout *hLay = new QHBoxLayout(5);
+   vLay->addLayout(hLay);
    //
    // Die eine oder zwei Zeile(n) mit der Passwortabfrage
    //
    QLabel *l_password = new QLabel(klocale->translate("Password"), this);
-   l_password->setGeometry( 10, 40, 80, 30 );
+   l_password->adjustSize();
+   l_password->setMinimumSize(l_password->size());
+   hLay->addWidget(l_password,0);
+   //l_password->setGeometry( 10, 40, 80, 30 );
    
    _w_password = new QLineEdit( this );
-   _w_password->setGeometry( 90, 40, 100, 30 );
+   //_w_password->setGeometry( 90, 40, 100, 30 );
    _w_password->setEchoMode( QLineEdit::Password );
+   _w_password->adjustSize();
+   _w_password->setFixedHeight(_w_password->height());
+   hLay->addWidget(_w_password,10);
    
    //
    // Connect vom LineEdit herstellen und Accelerator
@@ -60,17 +75,35 @@ PasswordDialog::PasswordDialog(const char *head, QWidget* parent, const char* na
    f->setLineWidth(1);
    f->setMidLineWidth(1);
    f->setFrameStyle( QFrame::HLine|QFrame::Raised);
-   f->setGeometry( 10, 80, 180, 2 );
+   //f->setGeometry( 10, 80, 180, 2 );
+   f->setMinimumHeight(5);
+   vLay->addWidget(f,0);
    
    //
    // Die Buttons "OK" & "Cancel" erzeugen
    //
+   QHBoxLayout *hBLay = new QHBoxLayout(5);
+   vLay->addLayout(hBLay);
+
+   hBLay->addStretch();
+
    QPushButton *b1, *b2;
    b1 = new QPushButton(klocale->translate("OK"), this);
-   b1->setGeometry( 10, 90, 80, 30 );
+   //b1->setGeometry( 10, 90, 80, 30 );
+   b1->adjustSize();
+   b1->setFixedSize(b1->size());   
+   hBLay->addWidget(b1);
+
+   hBLay->addStretch();
+   //or hBLay->addSpacing(10);
    
    b2 = new QPushButton(klocale->translate("Cancel"), this);
-   b2->setGeometry( 110, 90, 80, 30 );
+   //b2->setGeometry( 110, 90, 80, 30 );
+   b2->adjustSize();
+   b2->setFixedSize(b2->size());   
+   hBLay->addWidget(b2);
+
+   hBLay->addStretch();
    
    // Buttons mit Funktionaliataet belegen
    connect( b1, SIGNAL(clicked()), SLOT(accept()) );
@@ -82,7 +115,9 @@ PasswordDialog::PasswordDialog(const char *head, QWidget* parent, const char* na
    // Focus
    _w_password->setFocus();
    
-   setGeometry( x(), y(), 200, 130 );
+   vLay->activate();
+   setMinimumSize(200, 50);
+   setMaximumSize(300, 100);
 
    debugT("Hi folks, thats it\n");
 }
