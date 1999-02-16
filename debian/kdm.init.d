3d5f8881a574ae527b1e82c297cc4c8da220b491
KDE Dinosaur kulow <cvsuser@kde.org>
1999-02-16 23:11:57 +0000

    some more enhancements around kdm and menus

--- debian/kdm.init.d
+++ debian/kdm.init.d
@@ -1,7 +1,7 @@
 #!/bin/bash
 # /etc/init.d/xdm: start or stop XDM.
 
-test -x /usr/bin/X11/xdm || exit 0
+test -x /usr/bin/X11/kdm || exit 0
 
 test -f /etc/X11/config || exit 0
 
@@ -15,27 +15,13 @@ case "$1" in
         echo "WARNING : can only start kdm or xdm, but not both !"
     fi
 
-    echo -n 's/^SessionTypes=.*/SessionTypes=' > /tmp/kdm.sed
-    cat /etc/X11/window-managers |while read A; 
-    do
-        if test -x "$A"
-        then
-    	    echo -n "`basename $A`;" >> /tmp/kdm.sed
-        fi
-    done
-    echo -n "failsafe;/" >> /tmp/kdm.sed
-    cp /etc/kde/kdmrc /tmp/kdmrc.old
-    sed -f /tmp/kdm.sed < /tmp/kdmrc.old > /etc/kde/kdmrc
-    rm -f /tmp/kdm.sed /tmp/kdmrc.old
-
     echo -n "Starting kde display manager: kdm"    
     start-stop-daemon --start --quiet --exec /usr/bin/X11/kdm
     echo "."
     ;;
   stop)
       echo -n "Stopping kde display manager: kdm"    
-      start-stop-daemon --stop --quiet --exec /usr/bin/X11/kdm \
-		--pidfile /var/run/xdm-pid
+      start-stop-daemon --stop --quiet --pid /var/run/xdm-pid || echo " not running"
       echo "."
     ;;
 # the last options are taken from kerneld
