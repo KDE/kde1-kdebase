68c2f5d4444c5c1579e34e60d08eedf2533e890a
KDE Dinosaur esken <cvsuser@kde.org>
1999-03-11 23:55:12 +0000

    SuSE version of kcheckpass now in KDE1.1 branch

--- kcheckpass/checkpass_shadow.c
+++ kcheckpass/checkpass_shadow.c
@@ -1,4 +1,13 @@
 /*
+ * Wed Jan 27 14:25:45 MET 1999
+ *
+ * This is a modified version of checkpass_shadow.cpp
+ *
+ * Modifications made by Thorsten Kukuk <kukuk@suse.de>
+ *                       Mathias kettner <kettner@suse.de>
+ *
+ * ------------------------------------------------------------
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
@@ -16,50 +25,50 @@
  *      Copyright (C) 1998, Christian Esken <esken@kde.org>
  */
 #include "kcheckpass.h"
-#ifdef HAVE_SHADOW
-
 
 /*******************************************************************
  * This is the authentication code for Shadow-Passwords
  *******************************************************************/
 
+#ifdef HAVE_SHADOW
 #include <string.h>
+#include <pwd.h>
 
 #ifndef __hpux
 #include <shadow.h>
 #endif
 
-int authenticate(const char *login, const char *passwd)
+int authenticate(const char *login, const char *typed_in_password)
 {
-  struct passwd *pw;
-  char* crpt_passwd;
-  char* cp;
-  struct spwd *sp;
-  int result;
+  char          *crpt_passwd;
+  char          *password;
+  int           result;
 
-  pw = getpwnam(login);
-  if ( pw == 0 ) {
-    endspent();
-    return 2; /* cannot read password database */
-  }
+  struct passwd *pw= getpwnam(login);
+  if ( pw == 0 )
+    return 2;
+  
+  if (strcmp(pw->pw_passwd, "x") == 0) /* look up shadow */
+    {
+      struct spwd *spw = getspnam(login);
+      if ( spw == 0 ) 
+	return 2;
+      else password = spw->sp_pwdp;
+    }
   else
-   sp = getspnam(pw->pw_name);
-   if (sp)
-     cp = sp->sp_pwdp;
-   else
-     cp = pw->pw_passwd;
-
+    password = pw->pw_passwd;
+  
 #if defined( __linux__ ) && defined( HAVE_PW_ENCRYPT )
-  crpt_passwd = pw_encrypt(passwd, cp);  /* (1) */
+  crpt_passwd = pw_encrypt(typed_in_password, password);  /* (1) */
 #else  
-  crpt_passwd = crypt(passwd, cp);
+  crpt_passwd = crypt(typed_in_password, password);
 #endif
-  result = strcmp(cp, crpt_passwd);
+  result = strcmp(password, crpt_passwd );
   endspent();
   if (result == 0)
-    return 1; /* success */
+      return 1; /* success */
   else
-    return 0;
+      return 0;
 }
 
 /*
@@ -68,5 +77,4 @@ int authenticate(const char *login, const char *passwd)
      everything else you need to support shadow passwords is in
      the standard (ELF) libc.
  */
-
 #endif
