47c32c3c525e1f1d60036819272cab8cd00922f2
Helio Chissini de Castro <helio@kde.org>
2016-10-12 23:03:17 +0200

    kwmmodules kwmcom kcontrol/kdm kcontrol/info

--- kcontrol/input/keyboard.cpp
+++ kcontrol/input/keyboard.cpp
@@ -21,7 +21,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#include <iostream.h> 
+#include <iostream> 
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
